34b7f03c93c09afe2da9476d76544da83ae189ff
Anthony Lopez <anthony.lopez@unmsm.edu.pe>
2018-07-03 21:36:58 -0500

    fix endpoint principal y profile

--- controllers/home.js
+++ controllers/home.js
@@ -12,7 +12,7 @@ var db = require('../models/sequelize');
 
 exports.index = function(req, res) {
   var data = req.query
-  if (data.tipo == 'Docente'){
+  if (data.tipo == 'Docente' || data.tipo == 'docente' || data.tipo == 'DOCENTE'){
     db.User.findById(data.id)
     .then((user) => {
       console.log(user)
@@ -26,7 +26,7 @@ exports.index = function(req, res) {
       console.log(err)
     })
   }
-  else if (data.tipo == 'Administrador') {
+  else if (data.tipo == 'Administrador' || data.tipo == 'administrador' || data.tipo == 'ADMINISTRADOR') {
     req.flash('success', {msg: `Bienvenido al modulo Legajo Docente - FISI`})
     res.render('home', {
       title: 'Inicio'
